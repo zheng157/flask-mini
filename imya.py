fc1bd61be799fc9b5d261c436084206d5dbbc209
zheng157 <zzyyii1774@gmail.com>
2025-01-15 11:03:55 +0800

    add

--- imya.py
+++ imya.py
@@ -2,6 +2,6 @@ import requests
 import base64
 import re
 
-url = 'http://127.0.0.1:4723/wd/hub'
-response = requests.post(url).json()
-print(response)
+# url = 'http://127.0.0.1:4723/wd/hub'
+# response = requests.post(url).json()
+# print(response)
